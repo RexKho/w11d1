623ce3aa18a160878fbfdc7faf40bf59955a7595
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-12 17:06:53 -0800

    Fixed sizing issue

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -1,12 +1,31 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function Message({ size }) {
+  const [msgSize, setMsgSize] = useState('');
   useEffect(() => {
     console.log('Message', size);
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    console.log('cname', cname);
+    setMsgSize(cname);
   }, [size]);
 
   return (
-    <div className="message medium">
+    
+    <div className={`message ${msgSize}`}>
       (Oh my! Your bird is naked!)
     </div>
   );
